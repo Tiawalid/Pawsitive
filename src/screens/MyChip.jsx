f2a0ed468f4608c2e6b34ea3fd728a2cf4b66bdb
Tiawalid <168283519+Tiawalid@users.noreply.github.com>
2024-06-06 13:08:59 +0300

    Update MyChip.jsx

--- src/screens/MyChip.jsx
+++ src/screens/MyChip.jsx
@@ -1,25 +1,113 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View, TouchableOpacity, StyleSheet, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-const MyChip = () => {
+export default function MyChip() {
+  const navigation = useNavigation();
+
+  const handleMoodCheckerPress = () => {};
+
+  const handleRealTimeLocationPress = () => {};
+
+  const handleHealthMonitoringPress = () => {};
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My Chip</Text>
+      <View style={styles.header}>
+        <View style={styles.logoContainer}>
+          <Image
+            source={require("../../assets/images/logo.png")}
+            style={styles.logo}
+          />
+        </View>
+      </View>
+      <Text style={styles.title}>Hi, hope you're doing great!</Text>
+      <Text style={styles.subtitle}>
+        You can now use your chip to track your pet and ensure they are safe
+      </Text>
+      <View style={styles.content}>
+        <TouchableOpacity
+          style={[styles.button, styles.moodCheckerButton]}
+          onPress={handleMoodCheckerPress}
+        >
+          <Text style={styles.buttonText}>Mood Checker</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.button, styles.realTimeLocationButton]}
+          onPress={handleRealTimeLocationPress}
+        >
+          <Text style={styles.buttonText}>Real-Time Location</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.button, styles.healthMonitoringButton]}
+          onPress={handleHealthMonitoringPress}
+        >
+          <Text style={styles.buttonText}>Health Monitoring</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
+    backgroundColor: "#ADD8E6",
   },
-  text: {
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 16,
+  },
+  logoContainer: {
+    marginLeft: 10,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  title: {
     fontSize: 24,
     fontWeight: "bold",
+    textAlign: "center",
+    marginVertical: 10,
+    color: "#333",
+  },
+  subtitle: {
+    fontSize: 16,
+    textAlign: "center",
+    marginBottom: 10,
+    marginTop: 30,
+    color: "#333",
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 10,
+    width: "70%",
+    height: "15%",
+  },
+  moodCheckerButton: {
+    backgroundColor: "#6A5ACD",
+  },
+  realTimeLocationButton: {
+    backgroundColor: "#FF4500",
+  },
+  healthMonitoringButton: {
+    backgroundColor: "#32CD32",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
-
-export default MyChip;
